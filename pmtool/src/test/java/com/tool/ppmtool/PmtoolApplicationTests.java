fc8fa222c3f16decfa800397ea91ca116b1791d5
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-04-08 17:34:13 +0300

    first commit

---
+++ pmtool/src/test/java/com/tool/ppmtool/PmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tool.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
