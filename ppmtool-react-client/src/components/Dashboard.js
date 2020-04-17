954cce635ff8829f1266813a3de696870b805976
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-04-17 22:12:00 +0300

    React Frontend and Bootstrap added

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem';
+
+class Dashboard extends Component {
+    render() {
+        return (
+
+            <div>
+                <h1 className="allert alert-warning"> Welcome to the Dashboard</h1>
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+            </div>
+        
+            );  
+    }
+}
+
+export default Dashboard;
+
