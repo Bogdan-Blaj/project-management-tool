028771307f484a2a700496212ac50423e42a9eea
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-05-11 00:14:15 +0300

    changed typo from pmtool to ppmtool

--- ppmtool/src/main/java/com/tool/ppmtool/PpmtoolApplication.java
+++ ppmtool/src/main/java/com/tool/ppmtool/PpmtoolApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PmtoolApplication {
+public class PpmtoolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PmtoolApplication.class, args);
+		SpringApplication.run(PpmtoolApplication.class, args);
 	}
 
 }
