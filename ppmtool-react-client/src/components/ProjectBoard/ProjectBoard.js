79fe36b50df4ad41dd4307c9786b5f8b7b402e92
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-05-22 14:38:58 +0300

    BASIC CRUD for Project Tasks

--- ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
+import Backlog from './Backlog';
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {getBacklog} from "../../actions/backlogActions";
 
 class ProjectBoard extends Component {
+
+    //constructor to handle errors
+    constructor(){
+        super();
+        this.state = {
+            errors : {}
+        };
+    }
+
+    componentDidMount(){
+        const {id} = this.props.match.params;
+        this.props.getBacklog(id);
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(nextProps.errors){
+            this.setState({errors : nextProps.errors});
+        }
+    }
+
     
     render(){
         const {id} = this.props.match.params;
-        console.log("inside Project Board component")
+        const {project_tasks} = this.props.backlog;
+        const {errors} = this.state;
+        let BoardContent;
+
+    const boardAlgo = (errors, project_tasks) => {
+        if(project_tasks.length < 1){
+            if(errors.projectNotFound){
+                return (
+                    <div className="alert alert-danger text-center" role = "alert">
+                    {errors.projectNotFound}
+                    </div>
+                );
+            }
+            else {
+                return (
+                    <div className="alert alert-info text-center">
+                    No Project Task on this Board
+                    </div>
+                );
+            }
+        }
+        else {
+            return   <Backlog project_tasks_prop={project_tasks}/>;
+        }
+    };
+    BoardContent = boardAlgo(errors, project_tasks);
+
         return (
             <div className="container">
             <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -13,73 +63,8 @@ class ProjectBoard extends Component {
             </Link>
             <br />
             <hr />
-            {   
-            //  <!-- Backlog STARTS HERE -->
-            }
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <div className="card text-center mb-2">
-                            <div className="card-header bg-secondary text-white">
-                                <h3>TO DO</h3>
-                            </div>
-                        </div>
-    {
-                        // <!-- SAMPLE PROJECT TASK STARTS HERE -->
-                    }
-                        <div className="card mb-1 bg-light">
-    
-                            <div className="card-header text-primary">
-                                ID: projectSequence -- Priority: priorityString
-                            </div>
-                            <div className="card-body bg-light">
-                                <h5 className="card-title">project_task.summary</h5>
-                                <p className="card-text text-truncate ">
-                                    project_task.acceptanceCriteria
-                                </p>
-                                <a href=""  className="btn btn-primary">
-                                    View / Update
-                                </a>
-    
-                                <button className="btn btn-danger ml-4">
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-    
-                        {
-                            // <!-- SAMPLE PROJECT TASK ENDS HERE -->
-                        }
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card text-center mb-2">
-                            <div className="card-header bg-primary text-white">
-                                <h3>In Progress</h3>
-                            </div>
-                        </div>
-                    {                        
-                    // <!-- SAMPLE PROJECT TASK STARTS HERE -->
-    
-                    // <!-- SAMPLE PROJECT TASK ENDS HERE -->
-                    }
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card text-center mb-2">
-                            <div className="card-header bg-success text-white">
-                                <h3>Done</h3>
-                            </div>
-                        </div>
-                        {
-
-                        // <!-- SAMPLE PROJECT TASK STARTS HERE -->
-    
-                        // <!-- SAMPLE PROJECT TASK ENDS HERE -->
-                        }
-
-                    </div>
-                </div>
-            </div>
-    
+            {BoardContent}
+          
         </div>
     
             
@@ -88,4 +73,15 @@ class ProjectBoard extends Component {
     }
 }
 
-export default ProjectBoard;
+ProjectBoard.propTypes = {
+    getBacklog : PropTypes.func.isRequired,
+    backlog : PropTypes.object.isRequired,
+    errors : PropTypes.object.isRequired
+};
+
+const mapStateToProps = state =>({
+    backlog : state.backlog,
+    errors : state.errors
+});
+
+export default connect(mapStateToProps, {getBacklog})(ProjectBoard);
--- ppmtool-react-client/src/reducers/backlogReducer.js
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -22,7 +22,10 @@ export default function(state = initialState, action){
         
         case DELETE_PROJECT_TASK:
             return {
-                ...state
+                ...state,
+
+                //if the API call is succesfull
+                project_tasks: state.project_tasks.filter(project_task => project_task.projectSequence !== action.payload)
             }
         
         
